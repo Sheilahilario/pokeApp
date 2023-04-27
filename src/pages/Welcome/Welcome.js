900720c6740785883d4cd20f0f4f6ca0d21bc658
Sheilahilario <sheila.oh08@gmail.com>
2023-04-27 16:16:23 -0300

    feat: normalized pokemons

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -11,28 +11,42 @@ const Welcome = () => {
     const response = await instancePokeApi.get('/pokemon');
     setData(response.data.results);
     setName(response.data.name);
+
+    const normalizedResults = response.data.results.map((pokemon)=>{
+      const pokemonName= pokemon.name
+      const pokemonId = pokemon.url.replace("https://pokeapi.co/api/v2/pokemon/", "").replace("/", "") 
+      const normalizedPokemon = {name: pokemonName, id:pokemonId}
+
+    return(
+      normalizedPokemon
+    )
+    })
+    console.log(normalizedResults)
+    
   };
+
 
   useEffect(() => {
     getAllPokemons();
   }, []);
+  
+
     
-const renderItem = ({data, name}) => {
     return (
       <SafeAreaView style={{flex: 1}}>
         <Text style={styles.title}>Search Pokemons</Text>
-        <ScrollView>{data.map(renderItem)}</ScrollView>
+        {/* <ScrollView>{data.map(renderItem)}</ScrollView>
         <Image
           style={{ width: 150, height: 150 }}
           source={{
             uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
           }}
-        />
+        /> */}
       </SafeAreaView>
     );
   };
 
-}
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#262d31',
